2a0a672dd1b7f1d6866e704e3fccf8603bf4e173
Nícolas Ruwer Hackenhar <50345105+NicolasHackenhar@users.noreply.github.com>
2025-12-01 14:36:38 -0300

    feat(IXCTCPDF) Optimize file deletion logic in _destroy() method (#7)
    * feat(IXCTCPDF) Optimize file deletion logic in _destroy() method
    
    ---------
    
    Co-authored-by: Nícolas R. Hackenhar <nicolas.hackenhar@ixcsoft.com.br>

--- IXCTCPDF.php
+++ IXCTCPDF.php
@@ -8001,13 +8001,14 @@ class IXCTCPDF
         if ($destroyall and ! $preserve_objcopy && isset($this->file_id)) {
             self::$cleaned_ids[$this->file_id] = true;
             if (is_dir(K_PATH_CACHE)) {
-                $files = glob(K_PATH_CACHE.'__tcpdf_*');
+                $files = glob(K_PATH_CACHE . '__tcpdf_' . $this->file_id . '_*', GLOB_NOSORT);
                 foreach($files as $file) {
-                    if (is_file($file) && strpos(basename($file), '__tcpdf_' . $this->file_id . '_') === 0) {
+                    if (is_file($file)) {
                         unlink($file);
                     }
                 }
             }
+
             if (isset($this->imagekeys)) {
                 foreach ($this->imagekeys as $file) {
                     if (strpos($file, K_PATH_CACHE) === 0 && IXCTCPDF_STATIC::file_exists($file)) {
